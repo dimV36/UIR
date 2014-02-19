03cd004325df0b9c04a56bd7b7cee90ca317066a
Dmitry Voronin <carriingfate92@yandex.ru>
2014-02-19 17:47:38 +0400

    M2Crypto

---
+++ Диплом/SSLCerts/CA.py
@@ -0,0 +1,86 @@
+__author__ = 'dimv36'
+from M2Crypto import RSA, X509, m2, EVP
+
+
+from M2Crypto import RSA, X509, EVP, m2, Rand, ASN1
+
+# XXX Do I actually need more keys?
+# XXX Check return values from functions
+
+
+def generate_rsa_key():
+    return RSA.gen_key(2048, m2.RSA_F4)
+
+
+def make_public_key(key):
+    public_key = EVP.PKey()
+    public_key.assign_rsa(key)
+    return public_key
+
+
+def make_request(public_key):
+    request = X509.Request()
+    request.set_pubkey(public_key)
+    name = X509.X509_Name()
+    name.add_entry_by_txt(field='O', type=X509.ASN1.MBSTRING_ASC, entry='user@example', len=-1, loc=-1, set=0)
+    object = m2.obj_txt2obj("1.2.3.4.5", 200)
+    print(m2.obj_obj2nid(object))
+    print(object)
+
+
+#    m2.asn1_object_new("1.2.3.4.5", "SC", "SELinuxContext")
+#    print(X509.new_extension("SELinuxContext", "user_u:user_t:user_r:s0-s3", 1))
+#    print(X509.X509_Name.nid)
+#    X509.X509_Name.nid = X509.X509_Name.nid + {'SC', 30, 'SelinuxContext'}
+    request.set_subject_name(name)
+    extension = X509.new_extension('SC', 'Hello there')
+    extension_stack = X509.X509_Extension_Stack()
+    extension_stack.push(extension)
+    request.add_extensions(extension_stack)
+    request.sign(public_key, 'sha1')
+    return request
+
+
+def make_certificate(request, ca_public_key):
+    public_key = request.get_pubkey()
+    if not request.verify(public_key):
+        raise ValueError, 'Error verifying request'
+    subject = request.get_subject()
+    certificate = X509.X509()
+    certificate.set_serial_number(1)
+    certificate.set_version(2)
+    certificate.set_subject(subject)
+    issuer = X509.X509_Name()
+    issuer.CN = 'CA'
+    issuer.O = 'Test Organization'
+    certificate.set_issuer(issuer)
+    certificate.set_pubkey(public_key)
+    notBefore = m2.x509_get_not_before(certificate.x509)
+    notAfter = m2.x509_get_not_after(certificate.x509)
+    m2.x509_gmtime_adj(notBefore, 0)
+    m2.x509_gmtime_adj(notAfter, 60 * 60 * 24 * 30)
+    extension = X509.new_extension('nsComment', 'M2Crypto generated certificate', 1)
+    certificate.add_ext(extension)
+    certificate.sign(ca_public_key, 'sha1')
+    return certificate
+
+
+def ca():
+    rsa_key = generate_rsa_key()
+    public_key = make_public_key(rsa_key)
+    request = make_request(public_key)
+    certificate = make_certificate(request, public_key)
+    return (certificate, public_key)
+
+
+if __name__ == '__main__':
+    Rand.load_file('../randpool.dat', -1)
+    rsa_key = generate_rsa_key()
+    public_key = make_public_key(rsa_key)
+    request = make_request(public_key)
+    #print request.as_text()
+    certificate = make_certificate(request, public_key)
+    #print certificate.as_text()
+    #print certificate.as_pem()
+    certificate.save_pem('my_ca_cert.pem')
+    #rsa_key.save_key('my_key.pem', 'aes_256_cbc')
--- Диплом/SSLCerts/GenerateSSLCerts.py
+++ Диплом/SSLCerts/GenerateSSLCerts.py
@@ -1,83 +1,84 @@
 __author__ = 'dimv36'
-from OpenSSL import crypto, SSL
+from OpenSSL import *
+from M2Crypto import X509, RSA
 from socket import gethostname
 from pprint import pprint
 from time import gmtime, mktime
 from os.path import exists, join
 
+#
+# def generate_ca():
+#     key = RSA.new_pub_key()
+#     key.generate_key(crypto.TYPE_RSA, 2048)
+#     ca = crypto.X509()
+#     ca.set_version(3)
+#     ca.set_serial_number(1)
+#     ca.get_subject().CN = "main.ca.ru"
+#     ca.gmtime_adj_notBefore(0)
+#     ca.gmtime_adj_notAfter(24 * 60 * 60)
+#     ca.set_issuer(ca.get_subject())
+#     ca.set_pubkey(key)
+#     ca.add_extensions([crypto.X509Extension("basicConstraints", True, "CA:TRUE, pathlen:0"),
+#                       crypto.X509Extension("keyUsage", True, "keyCertSign, cRLSign"),
+#                       crypto.X509Extension("subjectKeyIdentifier", False, "hash", subject=ca),
+#                       crypto.X509Extension("SEContext", False, "context")])
+#     ca.sign(key, "sha1")
+#     open("cakey.pem", "wt").write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+#     open("cert.pem", "wt").write(crypto.dump_certificate(crypto.FILETYPE_PEM, ca))
+#
+#
+# def sign_certificate_by_ca():
+#     ca_certificate = crypto.load_certificate(crypto.FILETYPE_PEM, "ca.pem")
+#     ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM, "ca.pem")
+#     key = crypto.PKey()
+#     key.generate_key(crypto.TYPE_RSA, 2048)
+#     certificate = crypto.X509()
+#     certificate.get_subject().CN = "node1.example.com"
+#     certificate.set_serial_number(1)
+#     certificate.gmtime_adj_notBefore(0)
+#     certificate.gmtime_adj_notAfter(24 * 60 * 60)
+#     certificate.set_issuer(ca_certificate.get_subject())
+#     certificate.set_pubkey(key)
+#     certificate.sign(ca_key, "sha1")
+#
+#
+# def generate_certificate_request():
+#     key = crypto.PKey()
+#     key.generate_key(crypto.TYPE_RSA, 2048)
+#     request = crypto.X509Req()
+#     request.get_subject().CN = "node1.example.com"
+#     request.set_pubkey(key)
+#     request.sign(key, "sha1")
+#     open("key.pem", "wr").write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+#     open("req.csr", "wr").write(crypto.dump_certificate_request(crypto.FILETYPE_PEM, request))
+#
+#
+# def generate_certificate_by_request():
+#     ca_certificate = crypto.load_certificate(crypto.FILETYPE_PEM, "ca.pem")
+#     ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM, "ca.pem")
+#     request = crypto.load_certificate_request(crypto.FILETYPE_PEM, open("req.csr").read())
+#     certificate = crypto.X509()
+#     certificate.set_subject(request.get_subject())
+#     certificate.set_serial_number(1)
+#     certificate.gmtime_adj_notBefore(0)
+#     certificate.gmtime_adj_notAfter(24 * 60 * 60)
+#     certificate.set_issuer(ca_certificate.get_subject())
+#     certificate.set_pubkey(request.get_pubkey())
+#     certificate.sign(ca_key, "sha1")
+#     open("cert.pem", "wr").write(crypto.dump_certificate(crypto.FILETYPE_PEM, certificate))
+#
+#
+# def verify(key, cert):
+#     context = SSL.Context(SSL.TLSv1_METHOD)
+#     context.use_privatekey(key)
+#     context.use_certificate(cert)
+#     try:
+#         context.check_privatekey()
+#     except SSL.Error:
+#         print("Incorrect key")
+#     else:
+#         print("Key match certificate")
+#
 
-def generate_ca():
-    key = crypto.PKey()
-    key.generate_key(crypto.TYPE_RSA, 2048)
-    ca = crypto.X509()
-    ca.set_version(3)
-    ca.set_serial_number(1)
-    ca.get_subject().CN = "main.ca.ru"
-    ca.gmtime_adj_notBefore(0)
-    ca.gmtime_adj_notAfter(24 * 60 * 60)
-    ca.set_issuer(ca.get_subject())
-    ca.set_pubkey(key)
-    ca.add_extensions([crypto.X509Extension("basicConstraints", True, "CA:TRUE, pathlen:0"),
-                      crypto.X509Extension("keyUsage", True, "keyCertSign, cRLSign"),
-                      crypto.X509Extension("subjectKeyIdentifier", False, "hash", subject=ca),
-                      crypto.X509Extension("SEContext", False, "context")])
-    ca.sign(key, "sha1")
-    open("cakey.pem", "wt").write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
-    open("cert.pem", "wt").write(crypto.dump_certificate(crypto.FILETYPE_PEM, ca))
-
-
-def sign_certificate_by_ca():
-    ca_certificate = crypto.load_certificate(crypto.FILETYPE_PEM, "ca.pem")
-    ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM, "ca.pem")
-    key = crypto.PKey()
-    key.generate_key(crypto.TYPE_RSA, 2048)
-    certificate = crypto.X509()
-    certificate.get_subject().CN = "node1.example.com"
-    certificate.set_serial_number(1)
-    certificate.gmtime_adj_notBefore(0)
-    certificate.gmtime_adj_notAfter(24 * 60 * 60)
-    certificate.set_issuer(ca_certificate.get_subject())
-    certificate.set_pubkey(key)
-    certificate.sign(ca_key, "sha1")
-
-
-def generate_certificate_request():
-    key = crypto.PKey()
-    key.generate_key(crypto.TYPE_RSA, 2048)
-    request = crypto.X509Req()
-    request.get_subject().CN = "node1.example.com"
-    request.set_pubkey(key)
-    request.sign(key, "sha1")
-    open("key.pem", "wr").write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
-    open("req.csr", "wr").write(crypto.dump_certificate_request(crypto.FILETYPE_PEM, request))
-
-
-def generate_certificate_by_request():
-    ca_certificate = crypto.load_certificate(crypto.FILETYPE_PEM, "ca.pem")
-    ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM, "ca.pem")
-    request = crypto.load_certificate_request(crypto.FILETYPE_PEM, open("req.csr").read())
-    certificate = crypto.X509()
-    certificate.set_subject(request.get_subject())
-    certificate.set_serial_number(1)
-    certificate.gmtime_adj_notBefore(0)
-    certificate.gmtime_adj_notAfter(24 * 60 * 60)
-    certificate.set_issuer(ca_certificate.get_subject())
-    certificate.set_pubkey(request.get_pubkey())
-    certificate.sign(ca_key, "sha1")
-    open("cert.pem", "wr").write(crypto.dump_certificate(crypto.FILETYPE_PEM, certificate))
-
-
-def verify(key, cert):
-    context = SSL.Context(SSL.TLSv1_METHOD)
-    context.use_privatekey(key)
-    context.use_certificate(cert)
-    try:
-        context.check_privatekey()
-    except SSL.Error:
-        print("Incorrect key")
-    else:
-        print("Key match certificate")
-
-
-if __name__ == "__main__":
-    generate_ca()
+#if __name__ == "__main__":
+    # generate_ca()
