034548f95bd38318f9628999999712f162f933ec
Dmitry Voronin <carriingfate92@yandex.ru>
2014-03-25 22:51:00 +0400

    fix RPMS - без русского языка, проблемы с кодировкой

--- Diplom/SSLCerts/pgcert.py
+++ Diplom/SSLCerts/pgcert.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+__author__ = 'dimv36'
 from M2Crypto import RSA, X509, EVP, ASN1
 from subprocess import check_output
 from datetime import datetime
 from optparse import OptionParser
 from os import path, getuid
 from time import time
-__author__ = 'dimv36'
 
 
 DEFAULT_FIELDS = {'C': 'ru',
@@ -15,7 +13,7 @@ DEFAULT_FIELDS = {'C': 'ru',
                   'L': 'msk',
                   'O': 'mephi',
                   'OU': 'kaf36',
-                  'CN': check_output("whoami", shell=True)[0].split('\n')[0]}
+                  'CN': check_output("whoami", shell=True).split('\n')[0]}
 DEFAULT_PASSWORD = '123456'
 
 
@@ -25,22 +23,22 @@ def password(*args, **kwargs):
 
 def check_permissions():
     if getuid() != 0:
-        print('Пожалуйста, зайдите за пользователя `root` и повторите команду')
+        print("Please, login as `root` and try again")
         exit(1)
 
 
 def make_private_key(bits, output):
     rsa_key = RSA.gen_key(bits, 65537, callback=password)
     if not output:
-        output = path.abspath(path.curdir) + '/mykey.pem'
+        output = path.abspath(path.curdir) + "/mykey.pem"
     rsa_key.save_key(output, None)
-    return 'Сертификат сохранён в %s' % output
+    return 'Key was saved to %s' % output
 
 
 def make_request(private_key_file, username, context_string, output):
     private_key = EVP.load_key(private_key_file, callback=password)
     if not private_key:
-        raise ValueError, 'Неверный путь к приватному ключу'
+        raise ValueError, 'Not correct key path'
     request = X509.Request()
     request.set_pubkey(private_key)
     request.set_version(3)
@@ -54,23 +52,23 @@ def make_request(private_key_file, username, context_string, output):
     if context_string:
         context = context_string
     else:
-        context = check_output('id -Z', shell=True).split('\n')[0]
+        context = check_output("id -Z", shell=True).split('\n')[0]
     if not context:
-        raise ValueError, 'Команда`id -Z` возвратила ошибку'
+        raise ValueError, 'Command `id -Z` return with error code'
     name.SC = context
     request.set_subject_name(name)
     request.sign(private_key, 'sha1')
     if not output:
         output = path.abspath(path.curdir) + '/%s.csr' % DEFAULT_FIELDS['CN']
     request.save_pem(output)
-    return 'Запрос на подпись сертификата сохранён в %s' % output
+    return 'Request was saved to %s' % output
 
 
 def make_certificate(request_file, ca_private_key_file, ca_certificate_file, output):
     request = X509.load_request(request_file)
     public_key = request.get_pubkey()
     if not request.verify(public_key):
-        raise ValueError, 'Ошибка при проверке запроса на подпись сертификата'
+        raise ValueError, 'Error verifying request'
     subject = request.get_subject()
     ca_certificate = X509.load_cert(ca_certificate_file)
     ca_private_key = EVP.load_key(ca_private_key_file, callback=password)
@@ -87,39 +85,38 @@ def make_certificate(request_file, ca_private_key_file, ca_certificate_file, out
     certificate.set_not_after(not_after)
     certificate.set_issuer(issuer)
     certificate.set_pubkey(public_key)
-    certificate.add_ext(X509.new_extension('basicConstraints', 'CA:FALSE', 1))
+    certificate.add_ext(X509.new_extension("basicConstraints", "CA:FALSE", 1))
     if not output:
         output = path.abspath(path.curdir) + '/%s.cert' % DEFAULT_FIELDS['CN']
     certificate.sign(ca_private_key, 'sha1')
     certificate.save(output)
-    return 'Сертифкат сохранён в %s' % output
+    return 'Certificate was saved to %s' % output
 
 
 def verify_certificate(certificate_file, ca_certificate):
     certificate = X509.load_cert(certificate_file)
     if not certificate:
-        raise ValueError, 'Ошибка при загрузке сертифката'
+        raise ValueError, 'Error loading certificate file'
     ca_certificate = X509.load_cert(ca_certificate)
     ca_public_key = ca_certificate.get_pubkey()
     if not ca_certificate:
-        raise ValueError, 'Ошибка при загрузке приватного ключа пользователя'
+        raise ValueError, 'Error loading certificate key file'
     if certificate.verify(ca_public_key):
-        return 'Сертифкат достоверный'
+        return 'status verification ok'
     else:
-        pass
-    return 'Сертифкат не достоверный'
+        return 'status: verification failed'
 
 
 def print_certificate(certificate_file_path):
     if not path.exists(certificate_file_path):
-        raise ValueError, 'Путь к сертифкату %s не существует' % certificate_file_path
+        raise ValueError, 'Certificate path %s not exist' % certificate_file_path
     certificate = X509.load_cert(certificate_file_path)
     return certificate.as_text()
 
 
 def print_request(request_file_path):
     if not path.exists(request_file_path):
-        raise ValueError, 'Путь к запросу на подпись сертифката %s не существует' % request_file_path
+        raise ValueError, 'Request path %s not exist' % request_file_path
     request = X509.load_request(request_file_path)
     return request.as_text()
 
@@ -128,7 +125,7 @@ def make_ca(bits, cakey_file_path, cacert_file_path):
     make_private_key(bits, cakey_file_path)
     private_key = EVP.load_key(cakey_file_path, callback=password)
     if not private_key:
-        raise ValueError, 'Ошибка при загрзке приватного ключа удостоверяющего центра'
+        raise ValueError, 'Error loading CA private key'
     name = X509.X509_Name()
     name.C = DEFAULT_FIELDS['C']
     name.ST = DEFAULT_FIELDS['ST']
@@ -148,44 +145,41 @@ def make_ca(bits, cakey_file_path, cacert_file_path):
     not_after.set_datetime(datetime(datetime.today().year + 2, datetime.today().month, datetime.today().day))
     certificate.set_not_before(not_before)
     certificate.set_not_after(not_after)
-    certificate.add_ext(X509.new_extension('basicConstraints', 'CA:TRUE', 1))
+    certificate.add_ext(X509.new_extension("basicConstraints", "CA:TRUE", 1))
     certificate.sign(private_key, 'sha1')
     certificate.save(cacert_file_path)
-    return 'Сертифкат сохранён %s' % cacert_file_path
+    return 'Certificate was saved to %s' % cacert_file_path
 
 
 if __name__ == "__main__":
-    parser = OptionParser(usage="usage: %prog [--genrsa | --genreq | --gencert | --makeca | --text] options",
+    parser = OptionParser(usage="usage: %prog [--genrsa | --genreq | --gencert | --gencacert | --text] options",
                           add_help_option=True,
-                          description="Pgcert - утилита, позволяющая генерировать сертификаты X509 с полем SC, "
-                                      "в котором хранится текущий контекст пользователя, а также выполнять различные "
-                                      "операции с ними.")
+                          description="This program use M2Crypto library and can generate X509 certificate "
+                                      "with extension field SELinux Context")
     parser.add_option("--genrsa", dest="genrsa", action="store_true", default=False,
-                      help="Создать приватный RSA-ключ по количеству bits")
+                      help="generate private key with bits length")
     parser.add_option("--genreq", dest="genreq", action="store_true", default=False,
-                      help="Создать запрос на подпись сертификата по приватному ключу pkey")
+                      help="generate request for private key")
     parser.add_option("--gencert", dest="gencert", action="store_true", default=False,
-                      help="Создать сертификат пользователя по request")
+                      help="generate certificate for user")
     parser.add_option("--makeca", dest="makeca", action="store_true", default=False,
-                      help="Развернуть удостоверяющий центр")
+                      help="generate ca certificate and private key")
     parser.add_option("--text", dest="print_pem", action="store_true", default=False,
-                      help="Распечатать сертификат или запрос на подпись сертификата")
-    parser.add_option("--verify", dest="verify", action="store_true", default=False,
-                      help="Проверить, выдан ли сертификат пользователя данным удостоверяющим центром")
+                      help="print request or certificate")
+    parser.add_option("--verify", dest="verify", action="store_true", default=False, help="verify certificate")
     parser.add_option("--user", dest="user", default=DEFAULT_FIELDS['CN'],
-                      help="Добавить имя пользователя в запрос на подпись сертификата (опционально)")
-    parser.add_option("--context", dest="context", default=None,
-                      help="Добавить контекст пользователя в запрос на подпись сертфиката (опционально)")
+                      help="add username to certificate CN, default=%s" % DEFAULT_FIELDS['CN'])
+    parser.add_option("--context", dest="context", default=None, help="add user context to certificate")
     parser.add_option("--bits", dest="bits", type="int", default="2048",
-                      help="Длина приватного ключа (по умолчанию %default)")
-    parser.add_option("--request", dest="request", help="Путь к файлу запроса на подпись сертификата")
+                      help="bits for generate RSA-key, default=%default")
+    parser.add_option("--request", dest="request", help="add path to request file")
     parser.add_option("--cakey", dest="cakey", default="/etc/pki/CA/private/cakey.pem", type="string",
-                      help="Путь до приватного ключа удостоверяющего центра (по умолчанию %default)")
+                      help="add CA key path to generate user's certificate, default=%default")
     parser.add_option("--cacert", dest="cacert", default="/etc/pki/CA/cacert.pem", type="string",
-                      help="Путь к сертификату удостоверяющего центра (по умолчанию %default)")
-    parser.add_option("--pkey", dest="pkey", help="Путь к приватному ключу пользователя")
-    parser.add_option("--cert", dest="certificate", help="Путь к сертифкату")
-    parser.add_option("--output", type="string", dest="output", help="Путь к файлу вывода")
+                      help="add CA certificate path to generate user's certificate, default=%default")
+    parser.add_option("--pkey", dest="pkey", help="add path of private key")
+    parser.add_option("--cert", dest="certificate", help="add path of certificate")
+    parser.add_option("--output", type="string", dest="output", help="save to file output")
     options, args = parser.parse_args()
     user = options.user
     context = options.context
