f0bac912e96a22359458b9aa6362c7d051444e03
Dmitry Voronin <carriingfate92@yandex.ru>
2014-04-09 22:02:18 +0400

    extensions

--- Diplom/SRPMS/extensions/sslextensions--1.0.sql
+++ Diplom/SRPMS/extensions/sslextensions--1.0.sql
@@ -1,14 +1,11 @@
-CREATE OR REPLACE FUNCTION ssl_get_extension_by_name(text)
-RETURNS text AS
-'MODULE_PATHNAME', 'ssl_get_extension_by_name'
+CREATE FUNCTION ssl_get_extension_by_name(text) RETURNS text
+AS 'MODULE_PATHNAME', 'ssl_get_extension_by_name'
 LANGUAGE C STRICT;
 
-CREATE OR REPLACE FUNCTION ssl_is_critical_extension(text)
-RETURNS text AS
-'MODULE_PATHNAME', 'ssl_is_critical_extension'
+CREATE FUNCTION ssl_is_critical_extension(text) RETURNS text
+AS 'MODULE_PATHNAME', 'ssl_is_critical_extension'
 LANGUAGE C STRICT;
 
-CREATE OR REPLACE FUNCTION ssl_get_extensions_count()
-RETURNS text AS
-'MODULE_PATHNAME', 'ssl_get_extensions_count'
+CREATE FUNCTION ssl_get_extensions_count() RETURNS text
+AS 'MODULE_PATHNAME', 'ssl_get_extensions_count'
 LANGUAGE C STRICT;
--- Diplom/SRPMS/extensions/sslextensions.c
+++ Diplom/SRPMS/extensions/sslextensions.c
@@ -6,17 +6,17 @@
 #include "funcapi.h"
 
 #include <openssl/x509v3.h>
+#include <openssl/x509.h>
 
 PG_MODULE_MAGIC;
 
-Datum retcomposite(PG_FUNCTION_ARGS);
 Datum ssl_get_extension_by_name(PG_FUNCTION_ARGS);
 Datum ssl_is_critical_extension(PG_FUNCTION_ARGS);
 Datum ssl_get_extensions_count(PG_FUNCTION_ARGS);
 //Datum ssl_get_extensions_names(PG_FUNCTION_ARGS);
 
 
-X509_EXTENSION *get_extension(X509* certificate, char *name) {
+static X509_EXTENSION *get_extension(X509* certificate, char *name) {
 	int extension_nid = OBJ_sn2nid(name);
 	if (0 == extension_nid) {
 	    extension_nid = OBJ_ln2nid(name);
@@ -42,6 +42,13 @@ ssl_get_extension_by_name(PG_FUNCTION_ARGS)
 	if (NULL == certificate)
 	    PG_RETURN_NULL();
 	
+/*	int extension_nid = OBJ_sn2nid(extension_name);
+	if (0 == extension_nid) {
+	    elog(ERROR, "Could not get OID for \"%s\"", extension_name);
+	    PG_RETURN_NULL();
+	}
+	int locate = X509_get_ext_by_NID(certificate, extension_nid, -1);
+	extension = X509_get_ext(certificate, locate); */
 	extension = get_extension(certificate, extension_name);
 	if (NULL == extension) 
 	    elog(ERROR, "Extension by name \"%s\" is not found in certificate", extension_name);
@@ -53,7 +60,7 @@ ssl_get_extension_by_name(PG_FUNCTION_ARGS)
 	
 	result = cstring_to_text(value);
 	BIO_free(bio);
-	pfree(extension_name);
+	OPENSSL_free(extension);
 	
 	PG_RETURN_TEXT_P(result);
 }
@@ -70,9 +77,17 @@ ssl_is_critical_extension(PG_FUNCTION_ARGS) {
 	  PG_RETURN_NULL();
 	
 	extension = get_extension(certificate, extension_name);
+/*	int extension_nid = OBJ_sn2nid(extension_name);
+	if (0 == extension_nid) {
+	    elog(ERROR, "Could not get OID for \"%s\"", extension_name);
+	    PG_RETURN_NULL();
+	}
+	int locate = X509_get_ext_by_NID(certificate, extension_nid, -1);
+	extension = X509_get_ext(certificate, locate); */
 	if (NULL == extension) 
 	    elog(ERROR, "Extension name \"%s\" is not found in certificate", extension_name);
 	int critical = extension -> critical;
+	OPENSSL_free(extension);
 	
 	PG_RETURN_BOOL(critical > 0);
 }
@@ -82,11 +97,12 @@ PG_FUNCTION_INFO_V1(ssl_get_extensions_count);
 Datum
 ssl_get_extensions_count(PG_FUNCTION_ARGS) {
 	X509 *certificate = MyProcPort -> peer;
-	
+	elog(INFO, "certiciate is found");
 	if (NULL == certificate)
 	  PG_RETURN_NULL();
-	
+	elog(INFO, "certificate not null");
 	int extension_count = X509_get_ext_count(certificate);
+	elog(INFO, "count %d", extension_count);
 	PG_RETURN_INT32(extension_count);	
 }
 
